026e6f972deb2dbc5e577c22d99480f57a453c28
Todd Griffin <nemagnusfrater@gmail.com>
2018-05-27 04:49:13 -0700

    Add resources folder filled with profile pictures. Create initial script to dynamically load a given profile picture.

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const pics = [
+    "hackathon.jpeg",
+    "bowlgamestaircasepose.jpg",
+    "bowlgamedragonpose.jpg",
+    "cliffsofmohersitting.jpg",
+    "cliffsofmohersilhouette.jpg",
+    "sousa5kheldup.jpg",
+    "sousa5kfinishedpose.jpg",
+    "sousa5kfinishedheldup.jpg"
+]
+
+const profilePicture = document.getElementById('profilePicture');
+profilePicture.src = "res/" + pics[0];
